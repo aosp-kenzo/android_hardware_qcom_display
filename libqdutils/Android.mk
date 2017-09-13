1887b2c2600c5a2e5970d9f2ff536b5549011342
cb <billchen1977@gmail.com>
2017-09-13 17:54:56 +0800

    Move QCOMM display to vendor partition

--- libqdutils/Android.mk
+++ libqdutils/Android.mk
@@ -3,6 +3,7 @@ include $(LOCAL_PATH)/../common.mk
 include $(CLEAR_VARS)
 
 LOCAL_MODULE                  := libqdutils
+LOCAL_PROPRIETARY_MODULE      := true
 LOCAL_MODULE_TAGS             := optional
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libui libbinder libqservice
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
@@ -35,5 +36,6 @@ LOCAL_SRC_FILES                 += legacy.cpp
 endif
 LOCAL_MODULE_TAGS               := optional
 LOCAL_MODULE                    := libqdMetaData
+LOCAL_PROPRIETARY_MODULE        := true
 include $(BUILD_SHARED_LIBRARY)
 
